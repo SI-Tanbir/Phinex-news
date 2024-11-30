68725c56a15949b21a809347cadce65aeae965c3
SI-tanbir <si017490@gmail.com>
2024-11-30 23:12:15 +0600

    addding date with node pacage name moment ...that's awesome

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,22 @@
 
 import React from 'react'
 
+
 import headerLogo from '../../../assets/logo.png'
+import moment from 'moment/moment'
 
 const Header = () => {
   return (
-    <div> <img className=' m-auto' src={headerLogo} alt="header_logo" /></div>
+    <div> <img className=' m-auto' src={headerLogo} alt="header_logo" />
+        <div className='text-center'>
+
+        <p>Journalism without Fear or Favour</p>
+        <p>{moment().format('dddd , MMMM Do YYYY')}</p>
+
+
+        </div>
+       
+    </div>
   )
 }
 
