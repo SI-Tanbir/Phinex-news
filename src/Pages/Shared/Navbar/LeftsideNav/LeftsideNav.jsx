bc4dd469d8d9c2ca6eb54651a65c0a5e12aed95b
SI-tanbir <si017490@gmail.com>
2024-12-01 11:48:58 +0600

    adding catagories list

--- src/Pages/Shared/Navbar/LeftsideNav/LeftsideNav.jsx
+++ src/Pages/Shared/Navbar/LeftsideNav/LeftsideNav.jsx
@@ -1,10 +1,33 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import categories from '../../../../demo-data/categories.json'
 
 const LeftsideNav = () => {
+const [data,setData]=useState([]);
+
+useEffect(()=>{
+
+  setData(categories)
+},[])
+
+console.log(data)
   return (
     
-    <div>LeftsideNav</div>
+    <div>
+      <h2>All Catagory</h2>
+      {
+        data.map(cat=>(
+          <div key={cat.id}><h2>{cat.name}</h2></div>
+        ))
+      }
+
+      <div>
+
+
+      </div>
+
+
+    </div>
   )
 }
 
